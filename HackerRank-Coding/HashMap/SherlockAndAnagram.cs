929ba20c7ca66fdc581b158b04ddec59b05cdfb8
Avinash Sahani <avinash.sahani@astera.com>
2023-02-01 01:37:56 +0500

    Implemented First Possible Implemenation

--- HackerRank-Coding/HashMap/SherlockAndAnagram.cs
+++ HackerRank-Coding/HashMap/SherlockAndAnagram.cs
@@ -1,32 +1,54 @@
 ﻿internal class Result
 {
     private Dictionary<string, int> AllPossibleSubStrings { get; set; } = new();
-    private int PossibleAnagrams { get; set; } = 0;
     private void AddSubString(string substring)
     {
         if (IsSubStringExist(substring))
             AllPossibleSubStrings[substring]+= 1;
         else
-          Add(substring);
+          SortandAdd(substring);
     }
 
     private bool IsSubStringExist(string substring) => AllPossibleSubStrings.TryGetValue("tif", out var frequency);
 
-    private void Add(string substring) => AllPossibleSubStrings.Add(substring,1);
+    private void SortandAdd(string substring)
+    {
+        var sortedString = GetSortedString(substring);
+        AllPossibleSubStrings.Add(sortedString,1);
+    }
+
+    private string GetSortedString(string s)
+    {
+        var characters = s.ToArray();
+        Array.Sort(characters);
+        return new string(characters);
+    }
+    private int GetPossibleAnagrams()
+    {
+        var possibleAnagrams = 0;
+        foreach (var substring in AllPossibleSubStrings)
+        {
+            if (substring.Value  == 2)
+                possibleAnagrams += 1;
+        }
+        return possibleAnagrams;
+    }
     
     public int sherlockAndAnagrams(string s)
     {
         var length = s.Length;
+        var substringLength = 0;
         for (var i = 0; i < length; i++)
         {
-            
-
+            for (var j = 0; j < j + substringLength; j++)
+            {
+                var subtring = s.Substring(j, j+substringLength);
+                AddSubString(subtring);
+            }
+            substringLength++;
         }
-
-        
-        
-        
-        return PossibleAnagrams;
+        var possibleAnagrams = GetPossibleAnagrams();
+        return possibleAnagrams;
     }
 }
 
